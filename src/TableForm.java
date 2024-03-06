9a7e176192906c84298391b5563c25ea1d4acb6d
frystak_jan <janfrystak@gmail.com>
2024-03-06 12:51:21 +0100

    Session work + working file chooser and writer

---
+++ src/TableForm.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class TableForm extends JFrame {
+
+    private JPanel panel;
+    private JTextArea textArea;
+    private JTextField textField;
+    private JButton button;
+    private JFileChooser fc = new JFileChooser(".");
+    private JMenuBar mainMenu = new JMenuBar();
+        private JMenu optionMenu = new JMenu("Actions");
+            private JMenuItem copyBtn = new JMenuItem("Copy");
+            private JMenuItem loadFileBtn = new JMenuItem("Load File");
+
+    public TableForm(){
+        InitWindow();
+        initAcctionListeners();
+        initMenu();
+    }
+
+    public void remakeText(){
+        textArea.setText(String.valueOf(getText()));
+        textField.setText("");
+    }
+    public StringBuilder getText(){
+        StringBuilder text = new StringBuilder();
+        text.append(textField.getText()).append("\n").append(textArea.getText());
+        return text;
+    }
+    public void InitWindow(){
+        setJMenuBar(mainMenu);
+        setTitle("PetrTest");
+        setSize(500, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public void initAcctionListeners(){
+        button.addActionListener(e-> {remakeText();
+        });
+    }
+
+    public static void main(String[] args) {
+        TableForm frame = new TableForm();
+        frame.setContentPane(frame.panel);
+        frame.setVisible(true);
+    }
+    public void initMenu(){
+        mainMenu.add(optionMenu);
+        optionMenu.add(copyBtn);
+        optionMenu.add(loadFileBtn);
+
+        copyBtn.addActionListener(e -> remakeText());
+        loadFileBtn.addActionListener(e -> chooseFile());
+    }
+    public void chooseFile(){
+        int result = fc.showOpenDialog(this);
+        StringBuilder text = new StringBuilder();
+        if (result == JFileChooser.APPROVE_OPTION) {
+            try(Scanner sc = new Scanner(new BufferedReader(new FileReader(fc.getSelectedFile())))){
+                while (sc.hasNextLine()) text.append(sc.nextLine()).append("\n");
+                textArea.setText(String.valueOf(text));
+            } catch (FileNotFoundException e) {
+                JOptionPane.showMessageDialog(this, "Couldn't find file");
+            }
+        }
+    }
+}
